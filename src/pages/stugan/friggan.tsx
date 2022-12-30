e62aec5d2c4d80a9858de84c91409cb451208c51
AlexAbrahamsson <Alex.abrahamsson@gmail.com>
2022-12-30 09:28:23 +0100

    sliders

---
+++ src/pages/stugan/friggan.tsx
@@ -0,0 +1,24 @@
+const Friggan = [
+    {
+        title: 'Friggan 1',
+        url: 'https://drive.google.com/uc?export=view&id=12aoFuI8tCzruxGSNRlpBDQGZfCWvjyHc',
+    },
+    {
+        title: 'Friggan 2',
+        url: 'https://drive.google.com/uc?export=view&id=12ZEKlXqYAU-78hrSrikcjPDXeoWuHrWx',
+    },
+    {
+        title: 'Friggan 3',
+        url: 'https://drive.google.com/uc?export=view&id=12Rv374XO0KFL0kflDI3ab3WVWiTshJqN',
+    },
+    {
+        title: 'Friggan 4',
+        url: 'https://drive.google.com/uc?export=view&id=13W6F8uYkfX3aO6Nt-T-_RRrGiFYeco6D',
+    },
+    {
+        title: 'Friggan 5',
+        url: 'https://drive.google.com/uc?export=view&id=14xplaaZIOI1GOcNZHcg75tLqLrMZcRbo',
+    },
+]
+
+export default Friggan
--- src/pages/stugan/stugan.tsx
+++ src/pages/stugan/stugan.tsx
@@ -1,15 +1,48 @@
 import react from 'react';
-import { Container } from 'react-bootstrap';
-
-
-
-
-
+import { Container, Row } from 'react-bootstrap';
+import Slider from 'react-touch-drag-slider';
+import Images from './images';
+import Friggan from './friggan';
 
 export default function Stugan() {
     return (
-        <Container>
-            <h1>Stugan</h1>
+        <Container style={{ height: '93vh' }}>
+            <Row style={{height: '50%'}}>
+                <Slider
+                    onSlideComplete={(i) => {
+                        console.log('finished dragging, current slide is', i);
+                    }}
+                    onSlideStart={(i) => {
+                        console.log('started dragging on slide', i);
+                    }}
+                    activeIndex={0}
+                    threshHold={100}
+                    transition={0.5}
+                    scaleOnDrag={true}
+                >
+                    {Images.map(({ url, title }, index) => (
+                        <img src={url} key={index} alt={title} />
+                    ))}
+                </Slider>
+            </Row>
+            <Row style={{height: '50%'}}>
+                <Slider
+                    onSlideComplete={(i) => {
+                        console.log('finished dragging, current slide is', i);
+                    }}
+                    onSlideStart={(i) => {
+                        console.log('started dragging on slide', i);
+                    }}
+                    activeIndex={0}
+                    threshHold={100}
+                    transition={0.5}
+                    scaleOnDrag={true}
+                >
+                    {Friggan.map(({ url, title }, index) => (
+                        <img src={url} key={index} alt={title} />
+                    ))}
+                </Slider>
+            </Row>
         </Container>
     );
 }
