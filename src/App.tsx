a4f40987bf7efc480e6676622eaa9d6c22491184
Alex <Alex.abrahamsson@gmail.com>
2023-11-07 19:46:25 +0100

    dfgfd

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
-import style from './App.module.scss';
 import React from 'react';
 import Home from './pages/home/home';
-import CvPage from './pages/cv/cvPage';
-import Stugan from './pages/stugan/stugan';
 import { Container } from 'react-bootstrap';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Footer from './components/footer';
-import Friggan from './pages/friggan/friggan';
-import Dasset from './pages/dasset/dasset';
-import AppPage from './pages/appPage/appPage';
+
+import style from './App.module.scss';
 
 function App() {
     return (
@@ -16,11 +12,11 @@ function App() {
             <Router>
                 <Switch>
                     <Route exact path="/" component={Home} />
-                    <Route path="/cv" component={CvPage} />
+                    {/* <Route path="/cv" component={CvPage} />
                     <Route path="/appPage" component={AppPage} />
                     <Route path="/stugan" component={Stugan} />
                     <Route path="/friggan" component={Friggan} />
-                    <Route path="/dasset" component={Dasset} />
+                    <Route path="/dasset" component={Dasset} /> */}
                 </Switch>
             </Router>
             <Footer/>
--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,34 +1,13 @@
-import react, { useEffect } from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
-import Style from './home.module.scss';
+import React from 'react';
 import ContentContainer from '../../components/contentContainer/contentContainer';
 
-export default function Home() {
-    const [windowWidth, setWindowWidth] = react.useState(window.innerWidth / 15 + 'vh');
+import Style from './home.module.scss';
 
-    useEffect(() => {
-        window.addEventListener('resize', () => {
-            setWindowWidth(window.innerWidth / 15 + 'vh');
-        });
-    }, []);
+export default function Home() {
 
     return (
-        <Container style={{ maxHeight: '93vh', maxWidth: '940px' }}>
-            <div className={Style.background}>
-                <img
-                    style={{ height: windowWidth, width: windowWidth }}
-                    src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/1231630/moon2.png'
-                    alt=''
-                />
-                <div className={Style.stars}></div>
-                <div className={Style.twinkling}></div>
-                <div className={Style.clouds}></div>
-                <ContentContainer backgroundColor='rgba(0,0,0,0.4)'>
-                    <p style={{ color: 'lime', zIndex: 1000}}>
-                        hej ljhdlfjldfslkdnskövnösm
-                    </p>
-                </ContentContainer>
-            </div>
-        </Container>
+        <ContentContainer backgroundColor={'rgba(0,0,0,0.4)'}>
+            <h1>Home</h1>
+        </ContentContainer>
     );
 }
