7436ec67c64aefc8fad23625ee7b48cba1869786
Alexander Abrahamsson <79103208+Alex-Abrahamsson@users.noreply.github.com>
2023-11-08 07:16:17 +0100

    yrd

--- src/components/topComponent/topComponent.tsx
+++ src/components/topComponent/topComponent.tsx
@@ -2,15 +2,33 @@ import React from 'react';
 import Style from './topComponent.module.scss';
 import ScrollingText from '../scrollingText/scrollingText';
 
-export default function TopComponent() {
+interface ITopComponent {
+    WelcomeText: boolean;
+    TopRow: string;
+    BottomRow: string;
+    Opacity?: number;
+}
+
+export default function TopComponent({ WelcomeText, TopRow, BottomRow, Opacity = 1 }: ITopComponent) {
+
+    if (!WelcomeText) {
+        return (
+            <div className={Style.MainContainer} style={{ opacity: Opacity, pointerEvents: Opacity === 1 ? 'auto' : 'none' }}>
+                <div className={Style.TextContainer}>
+                    <h1>ABOUT</h1>
+                    <h1>PORTFOLIO</h1>
+                    <h1>CONTACT</h1>
+                </div>
+            </div>
+        );
+    }
 
     return (
-        <div className={Style.MainContainer}>
+        <div className={Style.MainContainer} style={{ opacity: Opacity, pointerEvents: Opacity === 1 ? 'auto' : 'none' }}>
             <div className={Style.TextContainer}>
-                <h1>Alexander</h1>
+                <h1>{TopRow}</h1>
                 <ScrollingText />
-                <h1>Abrahamsson</h1>
-                {/* Arrow component to scroll 1 section down */}
+                <h1>{BottomRow}</h1>
             </div>
         </div>
     );
--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -5,10 +5,34 @@ import Style from './home.module.scss';
 import TopComponent from '../../components/topComponent/topComponent';
 
 export default function Home() {
+    const [page1Opacity, setPage1Opacity] = React.useState(1);
+    const [page2Opacity, setPage2Opacity] = React.useState(0);
+
+    const switchOpacity = () => {
+        if (page1Opacity === 1) {
+            setPage1Opacity(0);
+            setPage2Opacity(1);
+        } else {
+            setPage1Opacity(1);
+            setPage2Opacity(0);
+        }
+    };
+
+    //Switch pages with Opacity
 
     return (
         <ContentContainer>
-            <TopComponent />
+            <TopComponent TopRow='Alexander' BottomRow='Abrahamsson' Opacity={page1Opacity} WelcomeText={true}/>
+            <TopComponent TopRow='Malexander' BottomRow='Braxilander' Opacity={page2Opacity} WelcomeText={false}/>
+
+            <button 
+                className={Style.AboutButton}
+                onClick={() => {
+                    switchOpacity();
+                }}
+            >
+                <p>{`<< >>`}</p>
+            </button>
         </ContentContainer>
     );
 }
