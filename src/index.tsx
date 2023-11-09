8eba36eababd15cbf4e8b92fb24ce69610ed4254
Alexander Abrahamsson <79103208+Alex-Abrahamsson@users.noreply.github.com>
2023-11-09 18:44:43 +0100

    Test

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+
 // https://firebase.google.com/docs/web/setup#available-libraries
 // TODO: Add SDKs for Firebase products that you want to use
 
@@ -9,7 +10,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App';
-import NavBar from './components/navBar/navBar';
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -25,13 +25,13 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    {/* <NavBar/> */}
     <App />
   </React.StrictMode>
 );
